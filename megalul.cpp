f96e1ee372816021dbe1acdbe0070742d353ac47
hashep <gooboo712@gmail.com>
2023-12-19 19:29:52 +0900

    ezy
    yes

--- megalul.cpp
+++ megalul.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+    std::cout << "enter twoshit" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "the sum of " << v1 << "and " << v2
+              << "is " << v1 + v2 << std::endl;
     return 0;
 }
