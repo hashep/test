3917e1731c6131fa64f546fa690a2a2c420fdbe6
hashep <gooboo712@gmail.com>
2023-12-19 20:54:34 +0900

    fak

--- megalul.cpp
+++ megalul.cpp
@@ -2,10 +2,11 @@
 
 int main()
 {
-    std::cout << "enter twoshit" << std::endl;
+    std::cout << "enter two shits:";
+    std::cout << std::endl;
     int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "the sum of " << v1 << "and " << v2
-              << "is " << v1 + v2 << std::endl;
-    return 0;
+    std::cin >> v1;
+    std::cin >> v2;
+    std::cout << v1 << "+" << v2 << "is" << v1 + v2 << std::endl;
+    return 1;
 }
