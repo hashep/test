5a9c14ec3847ef3670a386c3b1ea917823792c3a
hashep <gooboo712@gmail.com>
2023-12-19 21:17:09 +0900

    ez

--- megalul.cpp
+++ megalul.cpp
@@ -2,11 +2,19 @@
 
 int main()
 {
-    std::cout << "enter two shits:";
+    std::cout << "hewwo, wowwd";
     std::cout << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1;
-    std::cin >> v2;
-    std::cout << v1 << "+" << v2 << "is" << v1 + v2 << std::endl;
-    return 1;
+    int v1 = 0, v2 = 0, v5 = 0;
+    std::cin >> v1 >> v2 >> v5;
+
+    std::cout << "fuck + fuck = " << v1 + v2 + v5 << std::endl;
+    int v3 = 0, v4 = 0;
+    std::cin >> v3;
+    std::cin >> v4;
+    std::cout << std::endl;
+    std::cout << "fuck + fuck = " << v4 + v3 << std::endl;
+    int v10 = 0;
+    std::cin >> v10;
+    std::cout << std::endl;
+    return 0;
 }
