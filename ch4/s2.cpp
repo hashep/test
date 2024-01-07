bd331eb45d38e42634233059d2fe2c503d24d086
hashep <gooboo712@gmail.com>
2024-01-07 23:29:30 +0900

    kkkk

--- ch4/s2.cpp
+++ ch4/s2.cpp
@@ -12,24 +12,24 @@ using std::vector;
 
 int main()
 {
-    auto vi = {0, 1, 2, 3, 4, 5, 6};
+     auto vi = {0, 1, 2, 3, 4, 5, 6};
 
-    /* ** A는 int에 대한 포인터이다 == A는 무언가의 주소(int*)이다 */
+     /* ** A는 int에 대한 포인터이다 == A는 무언가의 주소(int*)이다 */
 
-    int a = 1;
-    int b = ++a;
+     int a = 1;
+     int b = ++a;
 
-    cout << a << "\n"
-         << b << "\n"
-         << endl;
+     cout << a << "\n"
+          << b << "\n"
+          << endl;
 
-    int c = 1;
-    int d = c++; // a++ 남한테 자기 원래값 주고 지가 올라감
+     int c = 1;
+     int d = c++; // c++ 남한테 자기 원래값 주고 지가 올라감
 
-    cout << c << "\n"
-         << d << endl;
+     cout << c << "\n"
+          << d << endl;
 
-    // value check
-    int blank;
-    cin >> blank;
+     // value check
+     int blank;
+     cin >> blank;
 }
---
+++ ch5/q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::begin;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+     ; // null
+
+     // value check
+     int blank;
+     cin >> blank;
+}
