6891356447566e1387464b7931f0672c47ffe3dc
hashep <gooboo712@gmail.com>
2024-01-09 17:50:32 +0900

    fasfaa

---
+++ ch6/x6 copy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::begin;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+     void print(const char *cp);
+
+     int clac(int, int);
+     cout << clac << endl;
+
+     int sales(int price);
+
+     cout << sales << endl;
+
+     // value check
+     int blank;
+     cin >> blank;
+}
