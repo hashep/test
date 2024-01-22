9419ff7f597f7a517145fba3de9aa50d3b88a334
hashep <gooboo712@gmail.com>
2024-01-23 00:33:03 +0900

    ff

---
+++ ch12/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <map>
+#include <set>
+
+using std::accumulate;
+using std::begin;
+using std::cin;
+using std::copy;
+using std::cout;
+using std::end;
+using std::endl;
+using std::ends;
+using std::fill;
+using std::find;
+using std::flush;
+using std::ifstream;
+using std::list;
+using std::map;
+using std::pair;
+using std::runtime_error;
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+    // value check
+    int blank;
+    cin >> blank;
+}
