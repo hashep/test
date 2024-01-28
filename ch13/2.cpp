6fade57d955de876a6b4d78221cbc232a8683179
hashep <gooboo712@gmail.com>
2024-01-28 20:50:23 +0900

    fg

---
+++ ch13/2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <list>
+#include <map>
+#include <set>
+#include <memory>
+
+using std::accumulate;
+using std::begin;
+using std::cin;
+using std::copy;
+using std::cout;
+using std::end;
+using std::endl;
+using std::ends;
+using std::fill;
+using std::find;
+using std::flush;
+using std::ifstream;
+using std::list;
+using std::map;
+using std::pair;
+using std::runtime_error;
+using std::set;
+using std::shared_ptr;
+using std::size_t;
+using std::string;
+using std::vector;
+
+int main()
+{
+    int i = 42;
+    int &r = i;
+    /* int &&a = i; */
+
+    // value check
+    int blank;
+    cin >> blank;
+}
